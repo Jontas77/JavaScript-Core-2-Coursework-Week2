5369090b3b6de18158d9f0e6c6a38a764212df7a
Jonathan Heugh <mrjbheugh@gmail.com>
2021-03-04 16:35:51 +0200

    Made changes to D

--- exercises/D-todo-list/script.js
+++ exercises/D-todo-list/script.js
@@ -2,55 +2,25 @@ function todoList(todos) {
   let contentDiv = document.getElementById('content');
   let ulTag = document.createElement('ul');
   contentDiv.appendChild(ulTag);
-  let liTag1 = document.createElement('li');
-  let liTag2 = document.createElement('li');
-  let liTag3 = document.createElement('li');
-  let liTag4 = document.createElement('li');
-  ulTag.appendChild(liTag1);
-  ulTag.appendChild(liTag2);
-  ulTag.appendChild(liTag3);
-  ulTag.appendChild(liTag4);
+  
+  todos.forEach(element => {
+    let liTag = document.createElement('li');
+    ulTag.appendChild(liTag);
+    liTag.className = 'todoList';
+    liTag.innerHTML = `${element.todo}`;
+  });
 
-
-
-
-  // for (let i = 0; i < todos.length; i++) {
-  //   ulTag.innerHTML += `<li>${todos[i].todo}</li>`;
-  // }
-  liTag1.innerHTML = `${todos[0].todo}`;
-  liTag2.innerHTML = `${todos[1].todo}`;
-  liTag3.innerHTML = `${todos[2].todo}`;
-  liTag4.innerHTML = `${todos[3].todo}`;
-
-  liTag1.addEventListener( 'click', () => {
-    if (liTag1.setAttribute('style', 'text-decoration: line-through')) {
-      liTag1.removeAttribute('style');
-    }
-  }); 
-    liTag2.addEventListener( 'click', () => {
-      if (liTag2.setAttribute('style', 'text-decoration: line-through')) {
-        liTag2.removeAttribute('style');
+  document.querySelectorAll('.todoList').forEach(item => {
+    item.addEventListener('click', () => {
+      if (item.style.textDecoration = 'none') {
+        return item.style.textDecoration = 'line-through';
+      } else if (item.style.textDecoration = 'line-through') {
+        return item.style.textDecoration = 'none'
       }
     });
-      liTag3.addEventListener( 'click', () => {
-        if (liTag3.setAttribute('style', 'text-decoration: line-through')) {
-          liTag3.removeAttribute('style');
-        }
-      });
-        liTag4.addEventListener( 'click', () => {
-          if (liTag4.setAttribute('style', 'text-decoration: line-through')) {
-            liTag4.removeAttribute('style');
-          }
-        });
-  //  if (liTag1.style.textDecoration = 'none') {
-  //    return liTag1.style.textDecoration = 'line-through';
-  //  } else {
-  //    return liTag1.style.textDecoration = 'none';
-  //  }
+  });
   
   };
-
-  
 
 const todos = [
   {todo: "wash the dishes"}, 
