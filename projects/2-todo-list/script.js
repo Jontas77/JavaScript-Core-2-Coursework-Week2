50707f8a301b57ade829839d756248854bb0d66c
Jontas77 <68595133+Jontas77@users.noreply.github.com>
2021-03-06 08:25:53 +0200

    Worked on todolist

--- projects/2-todo-list/script.js
+++ projects/2-todo-list/script.js
@@ -1,8 +1,31 @@
 function populateTodoList(todos) {
   let list = document.getElementById("todo-list");
+  let span = document.createElement("span");
+  span.classList.add("todo");
+  let newTodo = document.createElement("li");
+  newTodo.innerText = "hey";
+  list.appendChild(newTodo);
+  newTodo.appendChild(span);
+
+  let completedBtn = document.createElement("span");
+  completedBtn.innerHTML = '<i class="fa fa-check" aria-hidden="true"></i>';
+   completedBtn.classList.add("badge");
+   completedBtn.classList.add("bg-primary");
+   completedBtn.classList.add("rounded-pill");
+  span.appendChild(completedBtn);
+
+  let deleteBtn = document.createElement("span");
+  deleteBtn.innerHTML = ' <i class="fa fa-trash" aria-hidden="true"></i>';
+  deleteBtn.classList.add("badge");
+  deleteBtn.classList.add("bg-primary");
+  deleteBtn.classList.add("rounded-pill");
+  span.appendChild(deleteBtn);
+
+  
   // Write your code to create todo list elements with completed and delete buttons here, all todos should display inside the "todo-list" element.
 }
-
+let todoBtn = document.querySelector("button");
+todoBtn.addEventListener("click", addNewTodo);
 // These are the same todos that currently display in the HTML
 // You will want to remove the ones in the current HTML after you have created them using JavaScript
 let todos = [
