e356495b295b6666e5ced3d5ade85ac174e83dc7
Jonathan Heugh <mrjbheugh@gmail.com>
2021-03-23 15:53:50 +0200

    Completed todolist

--- projects/2-todo-list/script.js
+++ projects/2-todo-list/script.js
@@ -1,31 +1,48 @@
+const todoBtn = document.querySelector("button");
+const todoList = document.getElementById("todo-list");
+const todoInput = document.getElementById("todoInput");
+const deleteBtn = document.createElement("i");
+const completedBtn = document.createElement("i");
+
 function populateTodoList(todos) {
-  let list = document.getElementById("todo-list");
-  let span = document.createElement("span");
-  span.classList.add("todo");
-  let newTodo = document.createElement("li");
-  newTodo.innerText = "hey";
-  list.appendChild(newTodo);
-  newTodo.appendChild(span);
+  
+  const todoDiv = document.createElement("div");
+  todoDiv.classList.add("todo");
+  todoList.appendChild(todoDiv);
 
-  let completedBtn = document.createElement("span");
-  completedBtn.innerHTML = '<i class="fa fa-check" aria-hidden="true"></i>';
-   completedBtn.classList.add("badge");
-   completedBtn.classList.add("bg-primary");
-   completedBtn.classList.add("rounded-pill");
-  span.appendChild(completedBtn);
+  const newTodo = document.createElement("li");
+  newTodo.innerText = todoInput.value;
+  newTodo.classList.add("list-group-item");
+  newTodo.classList.add("d-flex");
+  newTodo.classList.add("justify-content-between");
+  newTodo.classList.add("align-items-center");
+  newTodo.classList.add("todo-item");
+  todoDiv.appendChild(newTodo);
 
-  let deleteBtn = document.createElement("span");
-  deleteBtn.innerHTML = ' <i class="fa fa-trash" aria-hidden="true"></i>';
-  deleteBtn.classList.add("badge");
-  deleteBtn.classList.add("bg-primary");
-  deleteBtn.classList.add("rounded-pill");
-  span.appendChild(deleteBtn);
+  const spanContainer = document.createElement("span");
+  spanContainer.classList.add("badge");
+  spanContainer.classList.add("bg-primary");
+  spanContainer.classList.add("rounded-pill");
+  newTodo.appendChild(spanContainer);
 
   
+  completedBtn.classList.add("fa");
+  completedBtn.classList.add("fa-check");
+  completedBtn.classList.add("checkBtn");
+  spanContainer.appendChild(completedBtn);
+
+  
+  deleteBtn.classList.add("fa");
+  deleteBtn.classList.add("fa-trash");
+  deleteBtn.classList.add("trashBtn");
+  spanContainer.appendChild(deleteBtn);
+
   // Write your code to create todo list elements with completed and delete buttons here, all todos should display inside the "todo-list" element.
 }
-let todoBtn = document.querySelector("button");
-todoBtn.addEventListener("click", addNewTodo);
+
+// todoBtn.addEventListener("click", addNewTodo);
+deleteBtn.addEventListener('click', deleteTodo);
+completedBtn.addEventListener('click', completedTodo)
 // These are the same todos that currently display in the HTML
 // You will want to remove the ones in the current HTML after you have created them using JavaScript
 let todos = [
@@ -33,13 +50,29 @@ let todos = [
   { task: "Do the shopping", completed: false },
 ];
 
-populateTodoList(todos);
+
 
 // This function will take the value of the input field and add it as a new todo to the bottom of the todo list. These new todos will need the completed and delete buttons adding like normal.
 function addNewTodo(event) {
   // The code below prevents the page from refreshing when we click the 'Add Todo' button.
   event.preventDefault();
   // Write your code here... and remember to reset the input field to be blank after creating a todo!
+  populateTodoList(todos);
+  todoInput.value = "";
+}
+
+
+
+function deleteTodo(e) {
+  let item = this.parentNode;
+  let ul = item.parentNode;
+  ul.remove();
+}
+
+function completedTodo(e) {
+  let item = this.parentNode;
+  let ul = item.parentNode;
+  ul.classList.toggle('completed');
 }
 
 // Advanced challenge: Write a fucntion that checks the todos in the todo list and deletes the completed ones (we can check which ones are completed by seeing if they have the line-through styling applied or not).
